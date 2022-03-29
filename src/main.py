74ce298f91adfa3dc9e931787801def3cbdf0284
Yunusov <yunusovyf@gmail.com>
2022-03-29 23:36:14 +0500

    add logging

--- src/main.py
+++ src/main.py
@@ -1,8 +1,16 @@
 from fastapi import FastAPI
+import logging.config
+import os
 
 from src.routers import all
 from src.database.models import Base
 from src.database.database import engine
+
+if not os.path.isdir('src/logs'):
+    os.makedirs('src/logs')
+
+logging.config.fileConfig('src/logging.conf')
+logging.info(f'Конфигурация логов загружена')
 
 app = FastAPI()
 
